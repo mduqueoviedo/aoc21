1c6225311989015ae5fba2509f70a7b9010a5901
Marcos Duque <marcos.duque@ingka.ikea.com>
2021-12-01 17:30:04 +0100

    Not elegant but a bit more elegant at least

--- day1/first.js
+++ day1/first.js
@@ -2,15 +2,16 @@ const fs = require('fs')
 const input = fs.readFileSync('./input.txt', 'utf-8')
     .trim()
     .split('\n')
+    .map((line) => Number(line))
 
-let current = Number(input[0])
+let current = input[0]
 let increments = 0
 
 input.forEach((line) => {
-    if (Number(line) > current) {
+    if (line > current) {
         increments++
     }
-    current = Number(line)
+    current = line
 })
 
 console.log("Increments are", increments)
